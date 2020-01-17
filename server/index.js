1bdb4d82b498e772575de31e72c6f8e9362ed72d
Matt Winzer <matt.winzer@gmail.com>
2020-01-17 10:39:02 -0700

    added morgan and / route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('server working');
+})
 
 app.listen(PORT, () =>  console.log(`listening on port ${PORT}`));
